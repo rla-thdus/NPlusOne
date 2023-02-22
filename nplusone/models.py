11a3ef3254975868a7c1228e606a571333eb5c2e
rla_thdus <0228ksy@naver.com>
2023-02-22 21:11:10 +0900

    Feat: tag, uploadDay, writer, webtoon 모델 생성

--- nplusone/models.py
+++ nplusone/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+
+
+class UploadDay(models.Model):
+    day = models.CharField(max_length=1)
+
+
+class Writer(models.Model):
+    name = models.CharField(max_length=10, unique=True)
+
+
+class WebToon(models.Model):
+    title = models.CharField(max_length=100)
+    summary = models.CharField(max_length=200)
+    writer = models.OneToOneField(Writer, on_delete=models.CASCADE)
+    upload_date = models.ForeignKey(UploadDay, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
