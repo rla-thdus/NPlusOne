4e19383eb7edff7d32b29b797aafbb88fd9d0de1
rla_thdus <0228ksy@naver.com>
2023-02-23 21:14:54 +0900

    Feat: 일대일 관계의 테이블의 데이터를 가져오는 api 생성

--- nplusone/views.py
+++ nplusone/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from nplusone.models import WebToon
+
+
+class TestAPI(APIView):
+    def get(self, request):
+        for webtoon in WebToon.objects.all():
+            print(webtoon.writer.name)
+        return Response(status=status.HTTP_200_OK)
